1ea492bfa9ecd61d661c6d9b87b304411427d75d
Manish Ranjan <manish.ranjan@stigasoft.com>
2019-08-16 22:15:44 +0530

    first commit

---
+++ precache-manifest.e90b19fb1b4ea0907922e3af837ab671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add46e83aa96d83eebcc634b9242554c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeb94c9a755de0bf73f8",
+    "url": "/static/css/main.7423d55e.chunk.css"
+  },
+  {
+    "revision": "7798441e0c7ada685fdd",
+    "url": "/static/js/2.273e6811.chunk.js"
+  },
+  {
+    "revision": "aeb94c9a755de0bf73f8",
+    "url": "/static/js/main.7033172b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
